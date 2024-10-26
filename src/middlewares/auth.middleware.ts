48160b08c2e203e67a4e4eee39004b7c00d4403a
Сенников Данил <d.sennikov@banki.ru>
2024-10-26 22:23:38 +0300

    revert testing commits

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,10 +4,10 @@ import { HTTP_STATUSES, SETTINGS } from '../constants';
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 	const authHeader = req.headers.authorization?.split('Basic ')[1];
-	const credentials = `${SETTINGS.LOGIN}:${SETTINGS.PASSWORD}`;
 
 	if (authHeader) {
 		const decodedAuth = Buffer.from(authHeader, 'base64').toString();
+		const credentials = `${SETTINGS.LOGIN}:${SETTINGS.PASSWORD}`;
 
 		if (decodedAuth === credentials) {
 			next();
@@ -15,5 +15,5 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
 		}
 	}
 
-	res.send({authHeader, credentials});
+	res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
 };

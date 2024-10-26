6049952187b6366f0d9dbe8794cfc7084ebbd720
Сенников Данил <d.sennikov@banki.ru>
2024-10-26 23:06:21 +0300

    Revert "for test"
    This reverts commit fb5f2757ce3d31403f493879fce64506346d3a96.

--- src/modules/blogs/blogs.router.ts
+++ src/modules/blogs/blogs.router.ts
@@ -80,11 +80,11 @@ blogsRouter.delete('/:id', authMiddleware, async (req: ReqParams<{ id: string }>
 		const isDeleted = await blogsService.deleteBlogById(req.params.id);
 
 		if (!isDeleted) {
-			res.send(isDeleted);
+			res.status(HTTP_STATUSES.NOT_FOUND_404).send(isDeleted);
 			return;
 		}
 
-		res.send(isDeleted);
+		res.status(HTTP_STATUSES.NO_CONTENT_204).send(isDeleted);
 	} catch (e) {
 		res.sendStatus(HTTP_STATUSES.INTERNAL_SERVER_500);
 	}
